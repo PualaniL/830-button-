ae617e37828b794efed81e9bea099d0ad4954107
PualaniL <130610369+PualaniL@users.noreply.github.com>
2023-05-17 10:23:37 -0700

    added cool button

--- button830.py
+++ button830.py
@@ -1,12 +1,18 @@
-#
-import tkinter as tk
+#Imported tkinter library
+import tkinter as tk 
 
-#
+#If button_click() is executed, "Button clicked!" will be printed.
 def button_click():
     print("Button clicked!")
-#
-root = tk.Tk()
+#root is assigned the value returned by tk.Tk(), and the title of the root window is set to "Button Example".
+root= tk.Tk()
 root.title("Button Example")
 
+#A button is created from the attributes: parent 'root', and the command 'cutton-click()'
+button = tk.Button(root, text="Click Me!", command=button_click)
+button.pack()
+
+#Calling the main root 'main loop'
+root.mainloop()
 
 
